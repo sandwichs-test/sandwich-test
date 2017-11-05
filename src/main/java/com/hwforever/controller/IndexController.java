81a17d89e320506249eaac528a56010b8cac4db9
李凯 <kaili6084@163.com>
2017-11-05 19:26:56 +0800

    add six jsp

--- src/main/java/com/hwforever/controller/IndexController.java
+++ src/main/java/com/hwforever/controller/IndexController.java
@@ -5,9 +5,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping("/aa")
-    public String test(){
-        System.out.println("asdf");
-        return "hello";
+	@RequestMapping("/index")
+    public String index(){
+        return "index";
     }
+    
+	@RequestMapping("/addExample")
+    public String addExample(){
+        return "addExample";
+    }
+    
+	@RequestMapping("/componentCtrl")
+    public String componentCtrl(){
+        return "componentCtrl";
+    }
+    
+	@RequestMapping("/environmentInfoCtrl")
+    public String environmentInfoCtrl(){
+        return "environmentInfoCtrl";
+    }
+    
+	@RequestMapping("/modelCtrl")
+    public String test(){
+        return "modelCtrl";
+    }
+    
+	@RequestMapping("/projectCtrl")
+    public String projectCtrl(){
+        return "projectCtrl";
+    }
+    
+    
 }
