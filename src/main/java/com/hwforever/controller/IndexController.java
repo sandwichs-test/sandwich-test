9f9fd08c9d8b5e4c05959d6a44b7ac1fe9bcad25
ZhangQiufeng <zhangqiufeng1995@gmail.com>
2017-11-07 13:08:30 +0800

    good job

--- src/main/java/com/hwforever/controller/IndexController.java
+++ src/main/java/com/hwforever/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 	@RequestMapping("/index")
     public String index(){
+        System.out.println("good job");
         return "index";
     }
     
