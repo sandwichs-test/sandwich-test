2635b20d1cae6b10f9cf6fd55e8fe599c3080d59
ZhangQiufeng <zhangqiufeng1995@gmail.com>
2017-11-07 13:10:03 +0800

    创建master

--- src/main/java/com/hwforever/controller/IndexController.java
+++ src/main/java/com/hwforever/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 	@RequestMapping("/index")
     public String index(){
+        System.out.println("创建master");
         System.out.println("good job");
         return "index";
     }
