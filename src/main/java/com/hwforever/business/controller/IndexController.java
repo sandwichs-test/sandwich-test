b17484bbf618ff7785a27417335958083aa130b4
QH-20170912RMCZ\Administrator <zhangqiufeng@163.com>
2017-11-09 23:26:07 +0800

    no message

---
+++ src/main/java/com/hwforever/business/controller/IndexController.java
@@ -0,0 +1,39 @@
+package com.hwforever.business.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+    
+	@RequestMapping("/addExample")
+    public String addExample(){
+        return "addExample";
+    }
+    
+	@RequestMapping("/componentCtrl")
+    public String componentCtrl(){
+        return "componentCtrl";
+    }
+    
+	@RequestMapping("/environmentInfoCtrl")
+    public String environmentInfoCtrl(){
+        return "environmentInfoCtrl";
+    }
+    
+	@RequestMapping("/modelCtrl")
+    public String test(){
+        return "modelCtrl";
+    }
+    
+	@RequestMapping("/projectCtrl")
+    public String projectCtrl(){
+        return "projectCtrl";
+    }
+    
+    
+}
