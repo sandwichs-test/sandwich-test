59c0527f0d623d93c6260b172a33b418fea2f792
ZhangQiufeng <zhangqiufeng1995@gmail.com>
2017-11-10 17:01:12 +0800

    no message

---
+++ sql/sandwich-test_20171110.sql
@@ -0,0 +1,186 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : zhangqiufeng
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : sandwich-test
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2017-11-09 16:25:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for case
+-- ----------------------------
+DROP TABLE IF EXISTS `case`;
+CREATE TABLE `case` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(256) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `case_type` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `service_name` varchar(256) NOT NULL,
+  `protocol` int(11) NOT NULL,
+  `envir_id` int(11) NOT NULL,
+  `pro_id` int(11) NOT NULL,
+  `ele_id` int(11) NOT NULL,
+  `mod_id` int(11) DEFAULT NULL,
+  `nature` varchar(256) NOT NULL,
+  `platform` varchar(256) NOT NULL,
+  `request_message` text NOT NULL,
+  `response_message_hope` text NOT NULL,
+  `response_message_actually` text,
+  `description` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='案例表';
+
+-- ----------------------------
+-- Table structure for element
+-- ----------------------------
+DROP TABLE IF EXISTS `element`;
+CREATE TABLE `element` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(256) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `ename` varchar(256) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='组件表';
+
+-- ----------------------------
+-- Table structure for environment
+-- ----------------------------
+DROP TABLE IF EXISTS `environment`;
+CREATE TABLE `environment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `ip` varchar(100) NOT NULL,
+  `port` int(11) NOT NULL,
+  `url` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='环境信息表';
+
+-- ----------------------------
+-- Table structure for module
+-- ----------------------------
+DROP TABLE IF EXISTS `module`;
+CREATE TABLE `module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(256) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `pro_id` int(11) NOT NULL,
+  `ele_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='模块表';
+
+-- ----------------------------
+-- Table structure for project
+-- ----------------------------
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(256) DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  `ename` varchar(256) DEFAULT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='项目信息表'
+
+-- ----------------------------
+-- Table structure for pro_ele_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `pro_ele_rel`;
+CREATE TABLE `pro_ele_rel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pro_id` int(11) NOT NULL,
+  `ele_id` int(11) NOT NULL,
+  `addtime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='项目组件映射关系表';
+
+-- ----------------------------
+-- Table structure for result
+-- ----------------------------
+DROP TABLE IF EXISTS `result`;
+CREATE TABLE `result` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `serial_number` varchar(256) DEFAULT NULL,
+  `case_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `type` varchar(256) DEFAULT NULL,
+  `execute_date` datetime DEFAULT NULL,
+  `execute_time` int(11) DEFAULT NULL,
+  `failed_reason` varchar(256) DEFAULT NULL,
+  `response_code` varchar(256) DEFAULT NULL,
+  `request_message` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='执行结果信息表';
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `add_time` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+-- ----------------------------
+-- Table structure for user_group
+-- ----------------------------
+DROP TABLE IF EXISTS `user_group`;
+CREATE TABLE `user_group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `add_time` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组';
+
+-- ----------------------------
+-- Table structure for user_group_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `user_group_rel`;
+CREATE TABLE `user_group_rel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `add_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户与用户组映射关系表';
+
+-- ----------------------------
+-- Table structure for user_role_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role_rel`;
+CREATE TABLE `user_role_rel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色映射关系表';
--- src/main/java/com/hwforever/business/mapper/UserMapper.java
+++ src/main/java/com/hwforever/business/mapper/UserMapper.java
@@ -1,4 +1,9 @@
 package com.hwforever.business.mapper;
 
-public class UserMapper {
+/**
+ * @Author： ZhangQiufeng
+ * @Description：
+ * @Date： Created in 17:00 2017/11/10
+ */
+public interface UserMapper {
 }
