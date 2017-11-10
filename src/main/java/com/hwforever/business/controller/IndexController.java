f548c5dc3974912ca4353d72d20c257b6d4950a6
ZhangQiufeng <zhangqiufeng1995@gmail.com>
2017-11-10 09:32:40 +0800

    no message

--- src/main/java/com/hwforever/business/controller/IndexController.java
+++ src/main/java/com/hwforever/business/controller/IndexController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:src/main/java/com/hwforever/controller/IndexController.java
+
 package com.hwforever.controller;
 
 import org.springframework.stereotype.Controller;
@@ -38,44 +38,3 @@ public class IndexController {
 
 
 }
-=======
-package com.hwforever.business.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class IndexController {
-	@RequestMapping("/index")
-    public String index(){
-        return "index";
-    }
-    
-	@RequestMapping("/addExample")
-    public String addExample(){
-        return "addExample";
-    }
-    
-	@RequestMapping("/componentCtrl")
-    public String componentCtrl(){
-        return "componentCtrl";
-    }
-    
-	@RequestMapping("/environmentInfoCtrl")
-    public String environmentInfoCtrl(){
-        return "environmentInfoCtrl";
-    }
-    
-	@RequestMapping("/modelCtrl")
-    public String test(){
-        return "modelCtrl";
-    }
-    
-	@RequestMapping("/projectCtrl")
-    public String projectCtrl(){
-        return "projectCtrl";
-    }
-    
-    
-}
->>>>>>> 4de5e0503c6a0613f068c83699dccb375b7dec8d:src/main/java/com/hwforever/business/controller/IndexController.java
