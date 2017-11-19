69111ffa7c02f1e24e18ba7126ea5ec607fbe5fc
ZhangQiufeng <zhangqiufeng1995@gmail.com>
2017-11-20 01:07:54 +0800

    no message

--- src/main/java/com/hwforever/SandwichApp.java
+++ src/main/java/com/hwforever/SandwichApp.java
@@ -1,12 +1,9 @@
 package com.hwforever;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SandwichApp {
     public static void main(String[] args) {
         SpringApplication.run(SandwichApp.class,args);
